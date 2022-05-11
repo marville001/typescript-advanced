969346df17e636b6a1b2b4206ba6382d9a96218c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-11 23:21:12 +0300

    :sparkles: render project list

--- src/app.ts
+++ src/app.ts
@@ -70,6 +70,42 @@ function autobind(
     return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: "active" | "finished") {
+        this.templateElement = document.getElementById(
+            "project-list"
+        )! as HTMLTemplateElement;
+        this.hostElement = <HTMLDivElement>document.getElementById("app")!;
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul")!.id = listId;
+
+        this.element.querySelector("h2")!.textContent =
+            this.type.toUpperCase() + " PROJECTS";
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
+
 //ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
@@ -169,3 +205,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
