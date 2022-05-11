44c3739e6a97ff133a9f1fcdf91a86296ab13182
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-11 21:02:06 +0300

    :sparkles: auto bind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+
+//autobind decorator
+function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this)
+			return boundFn
+		}
+	}
+	return adjDescriptor
+	
+}
+
+
+//ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -23,12 +40,25 @@ class ProjectInput {
 		this.descriptionInputElement = this.element.querySelector("#description")! as HTMLInputElement;
 		this.peopleInputElement = this.element.querySelector("#people")! as HTMLInputElement;
 
+		this.configure();
         this.attach();
-    }
+	}
+
+	@autobind
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+			
+	}
+	
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler)
+	}
 
     private attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
-    }
+	}
+	
 }
 
 
